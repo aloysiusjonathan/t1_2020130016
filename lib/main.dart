c1d263abc9e9a1127047ed145bb4e29dca6b5cfc
aloysiusjonathan <113957107+aloysiusjonathan@users.noreply.github.com>
2023-10-08 19:22:56 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '2020130016 - Aloysius Jonathan',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255,7,54,46)),
